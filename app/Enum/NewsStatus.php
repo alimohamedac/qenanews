3adbf2cab73cea2752f673f13a187a278b48faba
alimohamedac <a.mohamed666999@gmail.com>
2022-03-23 00:42:34 +0200

    migration news&news_trans&comments

---
+++ app/Enum/NewsStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enum;
+
+abstract class NewsStatus
+{
+    const Deleted   = 'deleted';
+    const Enabled   = 'enabled';
+    const Disabled  = 'disabled';
+    const Pending   = 'pending';
+}
+
---
+++ database/migrations/2022_03_23_125151_create_news_translations_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNewsTranslationsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+
+		Schema::create('news_translations', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->integer('news_id')->unsigned();
+			$table->string('title')->nullable();
+			$table->mediumText('description')->nullable();
+            //$table->schemalessAttributes('extra_data');
+			$table->string('locale');
+			$table->tinyInteger('enabled')->unsigned()->default(1);
+
+            $table->unique(['news_id','locale']);
+            $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade');
+		});
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('news_translations');
+	}
+
+}
