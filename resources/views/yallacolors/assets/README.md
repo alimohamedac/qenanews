0ace55ef51fe8aae392b056a7f01a0087f7d515c
alimohamedac <a.mohamed666999@gmail.com>
2022-05-06 16:37:59 +0200

    new theme admin

--- resources/views/yallacolors/assets/README.md
+++
@@ -1,2 +0,0 @@
-when you want to clone this admin theme, you must clone it in folder named "yallacolors" in you laravel project path:
-public\adminThemes
--- resources/views/yallacolors/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- begin #footer -->
-<div id="footer" class="footer text-center">
-  Powered By <a href="https://yallagroup.biz" target="_blank">YallaGroup Business</a> &copy; Copyright {{ date('Y') }}. All rights reserved.
-</div>
-<!-- end #footer -->
